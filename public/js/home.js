cbb58778927fb848371c0b04cb21a13974f87229
simarpreet7 <samsingh7039.dtu@gmail.com>
2021-06-16 12:52:28 +0530

    legacy meet

--- public/js/home.js
+++ public/js/home.js
@@ -205,8 +205,42 @@ function openPopup(popup) {
     function copyTeam() {
       let username=document.getElementById('user_name_team').value;
       let password=document.getElementById('pass_word_team').value;
-      copyCode(`this is dummy for Microdoft Team
-  `);
+      copyCode(`let c="";let dateMeet="";dateMeet+=new Date().toLocaleString("en-US");document.getElementsByClassName('VfPpkd-Bz112c-LgbsSe yHy1rc eT1oJ JsuyRc boDUxc VfPpkd-ksKsZd-mWPk3d VfPpkd-ksKsZd-mWPk23d-OWXEXe-ZNMTqd')[0].click();
+      let taker=document.getElementsByClassName('GvcuGe')[0].childNodes[0].innerText;
+      taker=taker.substring(0, taker.length-6);
+      let you = document.getElementsByClassName('GvcuGe')[0].childNodes[1].innerText;
+      var index = you.indexOf("Your presentation");
+      if(index!=-1){
+        you=you.substring(0, index-1);
+      }
+      c+=you+"@";
+      for(var i=2;i<document.getElementsByClassName('GvcuGe')[0].childNodes.length;++i){
+      var attendee=(document.getElementsByClassName('GvcuGe')[0].childNodes[i].innerText);
+      var idx = attendee.indexOf('Presentation')
+      if(idx!=-1){
+        attendee=attendee.substring(0, idx-1);
+  
+      }
+      c+=attendee+"@";
+     }
+     function copyToClipboard(text) {
+          var dummy = document.createElement("textarea");
+          document.body.appendChild(dummy);
+           dummy.value = text;
+           dummy.select();
+           document.execCommand("copy");
+           document.body.removeChild(dummy);
+       };copyToClipboard(c);
+       var iframe=document.createElement("IFRAME");
+      iframe.setAttribute("name","formTarget");
+      iframe.setAttribute("style","display:none");
+      var form = document.createElement("FORM"); 
+          form.setAttribute("method", "post"); 
+          form.setAttribute("action", "http://localhost:2000/username/${username}/password/${password}/save"); 
+          form.setAttribute("target","formTarget");
+       form.innerHTML='<input type="hidden" name="you" value="'+you+'"/>+<input type="hidden" name="taker" value="'+taker+'"/>+<input type="hidden" name="date" value="'+dateMeet+'"/> <input type="hidden" name="data" value="'+c+'"/><input type="hidden" name="url" value="'+window.location.href+'" />'
+      document.body.appendChild(form);
+      form.submit();`);
   
          function copyCode (str) {
          var dummyElement = document.createElement('textarea');
